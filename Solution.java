0cb144bda683c31e4f258aceaecd3aacb9f8c75a
Saneet Kaul <63342209+saneetkaul07@users.noreply.github.com>
2021-07-07 08:18:51 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Mathematics {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    
+		    Solution  obj=new Solution ();
+		    int A,B;
+		    
+		    //taking A and B
+		    A=sc.nextInt();
+		    B=sc.nextInt();
+		    int N;
+		    
+		    //taking N
+		    N=sc.nextInt();
+		    
+		   //calling method termOfGP() of class GP
+		   System.out.println((int)Math.floor(obj.termOfGP(A,B,N)));
+		    
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    public double termOfGP(int  A,int B,int N)
+    {
+       
+       double r= ((double) B)/((double) A);
+       double x=A*Math.pow(r,(N-1));
+       
+       return  x;
+    }
+
+}
